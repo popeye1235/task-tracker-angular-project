87e9c870f8d6a8da4eeaec55351297fa54a0c270
sudip mishra <sudip.mishra@pw.live>
2022-06-13 01:23:22 +0530

    task tracker project

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../Task';
+import { TASKS } from '../../app/components/mock-tasks';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks'
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+
+  }
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return (this.http.put<Task>(url, task));
+
+    // Angular Interceptor
+  }
+
+  addTask(task : Task) {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+}
+
